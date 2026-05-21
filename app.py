6d2d80bdb8559646729cc3fc27a7c6503094e7b2
rvsraoiit <vijayignou1@gmail.com>
2026-05-22 02:02:14 +0530

    Fixed database creation

--- app.py
+++ app.py
@@ -226,9 +226,8 @@ def logout():
     logout_user()
 
     return redirect("/login")
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
